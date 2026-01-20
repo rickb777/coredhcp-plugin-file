e71414e01a9994bfde4be4a4e3a2e318825309de
Rick <rick.beton@gmail.com>
2026-01-20 00:04:14 +0000

    new ntp plugin
    Signed-off-by: Rick <1358735+rickb777@users.noreply.github.com>

--- cmds/coredhcp/main.go
+++ cmds/coredhcp/main.go
@@ -24,6 +24,7 @@ import (
 	pl_mtu "github.com/coredhcp/coredhcp/plugins/mtu"
 	pl_nbp "github.com/coredhcp/coredhcp/plugins/nbp"
 	pl_netmask "github.com/coredhcp/coredhcp/plugins/netmask"
+	pl_ntp "github.com/coredhcp/coredhcp/plugins/ntp"
 	pl_prefix "github.com/coredhcp/coredhcp/plugins/prefix"
 	pl_range "github.com/coredhcp/coredhcp/plugins/range"
 	pl_router "github.com/coredhcp/coredhcp/plugins/router"
@@ -69,6 +70,7 @@ var desiredPlugins = []*plugins.Plugin{
 	&pl_leasetime.Plugin,
 	&pl_mtu.Plugin,
 	&pl_nbp.Plugin,
+	&pl_ntp.Plugin,
 	&pl_netmask.Plugin,
 	&pl_prefix.Plugin,
 	&pl_range.Plugin,
